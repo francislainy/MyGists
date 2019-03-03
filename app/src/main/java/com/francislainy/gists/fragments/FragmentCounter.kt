e52e6e52671d519702406299ca254ceb5c7ed241
Francislainy <francislainy.campos@gmail.com>
2019-03-03 11:44:22 +0000

    Delete animation listener for donutProgress

--- app/src/main/java/com/francislainy/gists/fragments/FragmentCounter.kt
+++ app/src/main/java/com/francislainy/gists/fragments/FragmentCounter.kt
@@ -1,25 +1,24 @@
 package com.francislainy.gists.fragments
 
-import android.animation.Animator
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import android.os.Build.VERSION_CODES.O
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.VibrationEffect.DEFAULT_AMPLITUDE
 import android.os.VibrationEffect.createOneShot
 import android.os.Vibrator
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.DecelerateInterpolator
 import android.widget.Button
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.francislainy.gists.R
 import com.francislainy.gists.activities.MainActivity
 import com.francislainy.gists.util.FIRST
+import com.francislainy.gists.util.setColor
 import com.francislainy.gists.util.toast
 import com.github.lzyzsd.circleprogress.DonutProgress
 import kotlinx.android.synthetic.main.fragment_counter.*
@@ -30,7 +29,6 @@ private val LOG_TAG: String? = "FragmentCounter"
 class FragmentCounter : Fragment() {
 
     private var v: Vibrator? = null
-    private val listObjectAnimator = java.util.ArrayList<ObjectAnimator>()
 
     private lateinit var questionsList: ArrayList<Int>
     private var question: Int? = null
@@ -44,14 +42,19 @@ class FragmentCounter : Fragment() {
         super.onResume()
 
         (activity as MainActivity).displayToolbar(FIRST)
+
+        v = activity!!.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(com.francislainy.gists.R.layout.fragment_counter, container, false)
     }
 
+    private lateinit var donutTimer: DonutProgress
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        donutTimer = view.findViewById(R.id.donutTimer)
 
         questionsList = arrayListOf(5, 1, 15, 2) // List with questions that will appear for the user to guess
 
@@ -69,66 +72,50 @@ class FragmentCounter : Fragment() {
 
         loadAllPossibleAnswers()
 
-        for ((index, answer) in map[question!!]!!.withIndex()) {
+        populateButtonsWithAnswers()
 
-            btnList[index].text = answer
-        }
 
-        updateDonutProgress(50f) //todo
+        object : CountDownTimer(30000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+                donutTimer.text = (millisUntilFinished / 1000).toString()
+                donutTimer.progress = (millisUntilFinished / 1000).toFloat()
+            }
+
+            override fun onFinish() {
+                vibrateDevice()
+                v?.cancel()
+
+                donutTimer.apply {
+                    text = "done!"
+                    textColor = setColor(activity as MainActivity, R.color.white)
+                    innerBackgroundColor = setColor(activity as MainActivity, R.color.colorPrimary)
+                    unfinishedStrokeColor = setColor(activity as MainActivity, R.color.colorPrimary)
+                    finishedStrokeColor = setColor(activity as MainActivity, R.color.colorPrimary)
+                }
+            }
+
+        }.start()
+
+
+        updateDonutProgress()
     }
 
-    private fun updateDonutProgress(progressParam: Float) {
+    private fun updateDonutProgress() {
 
-        val progress: Float = if (progressParam >= 100) {
-            100f // this is just in case if the user is over the target so the Donut doenst go over 100%
-        } else {
-            progressParam
-        }
-
-        // Donut Progress Animation :)
         activity!!.runOnUiThread {
-            val anim = ObjectAnimator.ofFloat(donutTimer, "progress", 0f, progress)
+            val anim = ObjectAnimator.ofFloat(
+                donutTimer,
+                "progress",
+                0f,
+                30f
+            ) // Start on 30 seconds (set on xml - donut_max property)
             anim.interpolator = DecelerateInterpolator()
-            anim.duration = 1000
+            anim.duration = 500
             anim.start()
-
-            listObjectAnimator.add(anim)
-
-            val donutTimer = activity!!.findViewById(com.francislainy.gists.R.id.donutTimer) as DonutProgress
-
-            anim.addListener(object : Animator.AnimatorListener {
-                override fun onAnimationStart(animation: Animator) {
-                    Log.d(LOG_TAG, "onAnimationStart")
-                }
-
-                override fun onAnimationCancel(animation: Animator) {
-                    Log.d(LOG_TAG, "onAnimationCancel")
-                }
-
-                override fun onAnimationRepeat(animation: Animator) {
-                    Log.d(LOG_TAG, "onAnimationRepeat")
-                }
-
-                override fun onAnimationEnd(animation: Animator) {
-
-                    Log.d(LOG_TAG, "onAnimationEnd")
-
-                    if (progress >= 100) {
-
-                        donutTimer.innerBackgroundColor = ContextCompat.getColor(activity as MainActivity, R.color.colorPrimary)
-
-                    } else {
-
-                        donutTimer.innerBackgroundColor = ContextCompat.getColor(activity as MainActivity, android.R.color.transparent)
-                    }
-
-                }
-
-            })
         }
-
     }
-
 
     private fun loadAllPossibleAnswers() {
         populatePossibleAnswers(0, arrayListOf("1+4", "9+1", "13+2", "10+10"))
@@ -167,37 +154,20 @@ class FragmentCounter : Fragment() {
             indexQuestion++
 
 
-            for ((index, answer) in map[question!!]!!.withIndex()) { // question.value = answerList
+            populateButtonsWithAnswers()
 
-                btnList[index].text = answer
-            }
-
+        } else {
+            vibrateDevice()
         }
+
     }
 
+    private fun populateButtonsWithAnswers() {
+        for ((index, answer) in map[question!!]!!.withIndex()) {
 
-//    private fun setUpVibrator() {
-//        v = activity!!.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator?
-//
-//        object : CountDownTimer(3000, 1000) {
-//
-//            override fun onTick(millisUntilFinished: Long) {
-//                tvCounter?.text = "seconds remaining: " + millisUntilFinished / 1000
-//            }
-//
-//            override fun onFinish() {
-//                tvCounter?.text = "done!"
-//                vibrateDevice()
-//                v?.cancel()
-//            }
-//
-//        }.start()
-//    }
-
-//    private val onClickGoToNext = View.OnClickListener {
-//
-//        (activity as MainActivity).displayView(SECOND)
-//    }
+            btnList[index].text = answer
+        }
+    }
 
     private fun vibrateDevice() {
 
@@ -210,10 +180,10 @@ class FragmentCounter : Fragment() {
         }
     }
 
-//    override fun onStop() {
-//        super.onStop()
-//
-//        v?.cancel()
-//    }
+    override fun onStop() {
+        super.onStop()
+
+        v?.cancel()
+    }
 
 }
--- app/src/main/java/com/francislainy/gists/util/ExtensionHelper.kt
+++ app/src/main/java/com/francislainy/gists/util/ExtensionHelper.kt
@@ -2,7 +2,13 @@ package com.francislainy.gists.util
 
 import android.content.Context
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 fun Context.toast(message: String = "Clicked") {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun setColor(context: Context, colorId: Int) : Int {
+
+    return ContextCompat.getColor(context, colorId)
 }
